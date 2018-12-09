71c0e0da94fb9def03787b88781440dcdb942d3f
jinyoung.heo <jinyoung.heo77@gmail.com>
2018-12-09 01:34:52 -0800

    simplied but works

--- service.py
+++ service.py
@@ -5,6 +5,7 @@ import board
 import busio
 import adafruit_mcp9808
 import adafruit_bme280
+import math
 
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 mcp = adafruit_mcp9808.MCP9808(i2c_bus)
@@ -14,20 +15,10 @@ class MyWStation(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def current(self, q="90404", APPID="APPID"):
-        temp = mcp.temperature + 273.15
-        press = "{0:0.2F}".format(bme280.pressure)
         return {
-            "coord": {
-                "lon": -118.49,
-                "lat": 34.02
-            },
-            "main": {
-                "temp": temp,
-                "temp_min": temp - 1, 
-                "temp_max": temp + 1, 
-                "pressure": float(press),
-                "humidity": int(float(bme280.humidity)) 
-            },
+            "temperature": mcp.temperature,
+            "pressure": bme280.pressure,
+            "humidity": bme280.humidity,
             "wind": {
                 "speed": 1.06,
                 "deg": 17.0003
@@ -35,6 +26,6 @@ class MyWStation(object):
             "dt": int(time.time())
         }
 
-if __name__ == '__main__':
-    cherrypy.config.update({'server.socket_host': '0.0.0.0'})
-    cherrypy.quickstart(MyWStation())
+if __name__ == '__main__': 
+        cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+        cherrypy.quickstart(MyWStation())
