c5fc7163801c0c519334f3884353718d669431c8
Jinyoung Heo <jinyoung.heo77@gmail.com>
2018-12-10 23:23:02 -0800

    uWSGI integration

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
 import cherrypy
 from app import Root
 
-app = cherrypy.tree.mount(Root(), '/')
+cherrypy.config.update({'engine.autoreload.on': False})
+cherrypy.server.unsubscribe()
+cherrypy.engine.start()
 
-if __name__ == '__main__': 
-        cherrypy.config.update({'server.socket_host': '0.0.0.0'})
-        cherrypy.quickstart(Root())
+wsgiapp = cherrypy.tree.mount(Root())
