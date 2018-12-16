d6aa1b7434a50b2e62fe69a88d4880422a4b5234
Jinyoung Heo <jinyoung.heo77@gmail.com>
2018-12-16 15:01:49 -0800

    Update I2C adress

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ import math
 
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 mcp = adafruit_mcp9808.MCP9808(i2c_bus)
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c_bus, 0x76)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c_bus, 0x77)
 
 class Root(object):
     @cherrypy.expose
