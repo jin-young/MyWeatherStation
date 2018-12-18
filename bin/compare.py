83d217f900bae081dd9ec5276088e6e728faea3f
Jinyoung Heo <jinyoung.heo77@gmail.com>
2018-12-17 21:41:53 -0800

    add utility to display temperature from mcp9808 and bme280

---
+++ bin/compare.py
@@ -0,0 +1,16 @@
+import time
+import board
+import busio
+import adafruit_mcp9808
+import adafruit_bme280
+from Adafruit_LED_Backpack import SevenSegment
+
+i2c_bus = busio.I2C(board.SCL, board.SDA)
+mcp = adafruit_mcp9808.MCP9808(i2c_bus)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c_bus, 0x77)
+
+while(True):
+  print("MCP9808: ", mcp.temperature)
+  print("BME280:  ", bme280.temperature)
+
+  time.sleep(10)
