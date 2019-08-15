bbecaa30b81730e863dfc65c2b17971288c82a68
Jinyoung Heo <classpath@gmail.com>
2019-08-14 20:07:28 -0700

    update display

--- bin/display.py
+++ bin/display.py
@@ -2,29 +2,34 @@
 import time
 import board
 import busio
-import adafruit_mcp9808
+import sqlite3
+from os import path
+import contextlib
+
 from Adafruit_LED_Backpack import SevenSegment
 
-i2c_bus = busio.I2C(board.SCL, board.SDA)
-mcp = adafruit_mcp9808.MCP9808(i2c_bus)
-
+root_dir = path.dirname(path.dirname(path.abspath(__file__)))
 
 segment = SevenSegment.SevenSegment(address=0x70)
-
 # Initialize the display. Must be called once before using the display.
 segment.begin()
 
 while(True):
   segment.clear()
+  
+  with contextlib.closing(sqlite3.connect(root_dir + '/db/weather.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)) as conn:
+    cur = conn.cursor()
+    cur.execute('SELECT temperature FROM weather ORDER BY id DESC limit 1')
+    record = cur.fetchone()
+    
+    temp = record[0]
 
-  temp = mcp.temperature
+    segment.print_float(temp, 1, False)
+    segment.set_digit(3, 'C', False)        # Ones
 
-  segment.print_float(temp, 1, False)
-  segment.set_digit(3, 'C', False)        # Ones
-
-  # Write the display buffer to the hardware.  This must be called to
-  # update the actual display LEDs.
-  segment.write_display()
+    # Write the display buffer to the hardware.  This must be called to
+    # update the actual display LEDs.
+    segment.write_display()
 
   # Wait a quarter second (less than 1 second to prevent colon blinking getting$
   time.sleep(10)
